a2df458df4cec4bb55349dba2b3d6e720145f804
Norm Papernick <norm@nrec.ri.cmu.edu>
2024-10-29 23:15:58 -0400

    added keys placeholder

--- local_up.sh
+++ local_up.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ ! -e config/keys.yaml ]]; then
+    cp config/keys_placeholder.yaml config/keys.yaml
+fi
+
 docker compose -f docker-compose-local.yaml --env-file config.local.env down
 docker compose -f docker-compose-local.yaml  --env-file config.local.env up --build --remove-orphans
